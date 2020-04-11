6f6b6788f322bc463265db03d70e4e9b2244bb89
Truly Mittal <trulymittal@gmail.com>
2020-04-11 21:19:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admobexample/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.admobexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adView = findViewById(R.id.adView);
+
+        MobileAds.initialize(this);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+}
